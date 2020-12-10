87675cee62be6bf56b98629236c795c0bd4fee6e
shooooting <shooootingsta@gmail.com>
2020-12-10 19:41:03 +0900

    MVVM 패턴을 활용해서 firebase를 활용한 채팅앱 만들기

---
+++ ChatApp/Controller/Authentication/LoginController.swift
@@ -0,0 +1,86 @@
+//
+//  LoginController.swift
+//  ChatApp
+//
+//  Created by ㅇ오ㅇ on 2020/12/10.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let iconImage: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(systemName: "bubble.right")
+        iv.tintColor = .white
+        return iv
+    }()
+    
+    private let emailContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .cyan
+        view.setHeight(height: 50)
+        return view
+    }()
+    
+    private let passwordContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        view.setHeight(height: 50)
+        return view
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.backgroundColor = .systemRed
+        button.setHeight(height: 50)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    // MARK: - Helper
+    
+    func configureUI() {
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+        
+        configureGradientLayer()
+        
+        view.addSubview(iconImage)
+        iconImage.centerX(inView: view)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        iconImage.setDimensions(height: 120, width: 120)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,
+                                                   passwordContainerView,
+                                                   loginButton])
+        
+        stack.axis = .vertical
+        stack.spacing = 16
+        
+        view.addSubview(stack)
+        
+        stack.anchor(top: iconImage.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor,
+                     paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+    }
+    
+    func configureGradientLayer() {
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemPink.cgColor]
+        gradient.locations = [0, 1]
+        view.layer.addSublayer(gradient)
+        gradient.frame = view.frame
+    }
+}
---
+++ ChatApp/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationController.swift
+//  ChatApp
+//
+//  Created by ㅇ오ㅇ on 2020/12/10.
+//
+
+import Foundation
