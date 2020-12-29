611c6b99e7f09cbdfc68e64d16acbd890e8f06c9
shooooting <shooootingsta@gmail.com>
2020-12-29 20:27:30 +0900

    채팅내용 firestore에 저장하기 -> 상대방 이름도 같이 저장하기

---
+++ ChatApp/API/Units/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  ChatApp
+//
+//  Created by ㅇ오ㅇ on 2020/12/29.
+//
+
+import Firebase
+
+let COLLECTION_MESSAGES = Firestore.firestore().collection("messages")
--- ChatApp/Model/Message.swift
+++ ChatApp/Model/Message.swift
@@ -5,9 +5,23 @@
 //  Created by ㅇ오ㅇ on 2020/12/27.
 //
 
-import Foundation
+import Firebase
 
 struct Message {
     let text: String
+    let toId: String
+    let fromId: String
+    let timestamp: Timestamp!
+    var user: User?
+    
     let isFromCurrentUser: Bool
+    
+    init(dictionary: [String: AnyObject]) {
+        self.text = dictionary["text"] as? String ?? ""
+        self.toId = dictionary["toId"] as? String ?? ""
+        self.fromId = dictionary["fromId"] as? String ?? ""
+        self.timestamp = dictionary["timestamp"] as? Timestamp ?? Timestamp(date: Date())
+        
+        self.isFromCurrentUser = fromId == Auth.auth().currentUser?.uid
+    }
 }
